faace398f2e20282811e793975dcb1fc0c529d44
maksg67 <maksymilian.golik@student.wat.edu.pl>
2026-06-07 19:58:20 +0200

    Rozbudowa CRUD, ostateczne poprawki, filtracja

--- controller.py
+++ controller.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
-from model import domy, pracownicy, pensjonariusze
+import model
 
 
 def get_coordinates(location):
@@ -43,21 +43,39 @@ class Pensjonariusz:
 
 def load_data():
     obiekty_domy = []
-    for d in domy:
+    for d in model.domy:
         obiekty_domy.append(DomOpieki(d["nazwa"], d["lokalizacja"]))
 
     obiekty_prac = []
-    for p in pracownicy:
+    for p in model.pracownicy:
         dom = next((x for x in obiekty_domy if x.nazwa == p["dom"]), None)
         if dom:
             obiekty_prac.append(Pracownik(p["imie"], p["nazwisko"], p["wiek"], p["rola"], dom))
             dom.pracownicy.append(obiekty_prac[-1])
 
     obiekty_pens = []
-    for p in pensjonariusze:
+    for p in model.pensjonariusze:
         dom = next((x for x in obiekty_domy if x.nazwa == p["dom"]), None)
         if dom:
             obiekty_pens.append(Pensjonariusz(p["imie"], p["nazwisko"], p["wiek"], p["choroby"], dom))
             dom.pensjonariusze.append(obiekty_pens[-1])
 
     return obiekty_domy, obiekty_prac, obiekty_pens
+
+
+def save_model(domy, pracownicy, pensjonariusze):
+    with open("model.py", "w", encoding="utf-8") as f:
+        f.write("domy = [\n")
+        for d in domy:
+            f.write(f"    {{'nazwa': '{d.nazwa}', 'lokalizacja': '{d.lokalizacja}'}},\n")
+        f.write("]\n\n")
+
+        f.write("pracownicy = [\n")
+        for p in pracownicy:
+            f.write(f"    {{'imie': '{p.imie}', 'nazwisko': '{p.nazwisko}', 'wiek': {p.wiek}, 'rola': '{p.rola}', 'dom': '{p.dom.nazwa}'}},\n")
+        f.write("]\n\n")
+
+        f.write("pensjonariusze = [\n")
+        for p in pensjonariusze:
+            f.write(f"    {{'imie': '{p.imie}', 'nazwisko': '{p.nazwisko}', 'wiek': {p.wiek}, 'choroby': '{p.choroby}', 'dom': '{p.dom.nazwa}'}},\n")
+        f.write("]\n")
