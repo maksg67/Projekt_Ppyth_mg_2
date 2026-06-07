80e48424843b3be7b424c902b383e90e97a3d831
maksg67 <maksymilian.golik@student.wat.edu.pl>
2026-06-07 18:58:41 +0200

    Podstawy projektu, model danych domy, pracownicy i pensjonariusze

---
+++ model.py
@@ -0,0 +1,14 @@
+domy = [
+    {"nazwa": "Dom1", "lokalizacja": "Warszawa"},
+    {"nazwa": "Chata2", "lokalizacja": "Gdańsk"},
+    {"nazwa": "Pensjonat3", "lokalizacja": "Kraków"}
+]
+
+pracownicy = [
+    {"imie": "Tomasz", "nazwisko": "Tomaszewski", "wiek": 45, "rola": "Pielęgniarz", "dom": "Dom1"}
+]
+
+pensjonariusze = [
+    {"imie": "Adam", "nazwisko": "Nowak", "wiek": 77, "choroby": "brak", "dom": "Dom1"},
+    {"imie": "Mateusz", "nazwisko": "Wiśniewski", "wiek": 88, "choroby": "Parkinson", "dom": "Pensjonat3"}
+]
